21b38396c71e07d4602832f1d5b606606c56a084
kaitoukid1002 <kaitoukid_1002@yahoo.com.vn>
2016-11-24 09:49:12 +0700

    Update web-service.py
    Add function display image

--- Music/web-service.py
+++ Music/web-service.py
@@ -7,25 +7,27 @@ root = tree.getroot()
 urls = (
     '/users', 'list_users',
     '/users/(.*)', 'get_user',
-    '/upload', 'Upload'
+    '/upload', 'Upload',
+    '/result','result'
 )
 app = web.application(urls, globals())
 
-
+file_path = ""
+file_name = ""
 class list_users:
     def GET(self):
-	output = 'users:[';
-	for child in root:
-                print 'child', child.tag, child.attrib
-                output += str(child.attrib) + ','
-	output += ']';
+        output = 'users:[';
+        for child in root:
+            print 'child', child.tag, child.attrib
+            output += str(child.attrib) + ','
+        output += ']';
         return output
 
 class get_user:
     def GET(self, user):
-	for child in root:
-		if child.attrib['id'] == user:
-		    return str(child.attrib)
+        for child in root:
+            if child.attrib['id'] == user:
+                return str(child.attrib)
 
 class Upload:
     def GET(self):
@@ -39,16 +41,26 @@ class Upload:
 </body></html>"""
 
     def POST(self):
+        global file_path
+        global file_name
         x = web.input(myfile={})
         filedir = '/home/quytx/webservice'  # change this to the directory you want to store the file in.
         if 'myfile' in x:  # to check if the file-object is created
             filepath = x.myfile.filename.replace('\\', '/')  # replaces the windows-style slashes with linux ones.
             filename = filepath.split('/')[-1]  # splits the and chooses the last part (the filename with extension)
             fout = open(filedir + '/' + filename, 'w')  # creates the file where the uploaded file should be stored
+            file_name = filename
+            file_path = filedir +"/"+ filename
             fout.write(x.myfile.file.read())  # writes the uploaded file to the newly created file.
             fout.close()  # closes the file, upload complete.
-        raise web.seeother('/upload')
+        raise web.seeother('/result')
 
+class result:
+    def GET(self):
+        global file_path
+        global file_name
+        imageBinary = open (file_path,"rb").read()
+        return  imageBinary
 
 if __name__ == "__main__":
     app.run()
